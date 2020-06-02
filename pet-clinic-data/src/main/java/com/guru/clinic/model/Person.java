956da73a06bb9f4955d9416555e57aeeaf16488c
KobloshAlex <Koblosh.Alex@gmail.com>
2020-06-01 23:21:50 -0400

    POJO

--- pet-clinic-data/src/main/java/com/guru/clinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/guru/clinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.guru.clinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/guru/clinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/guru/clinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.guru.clinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDate dateBirth;
