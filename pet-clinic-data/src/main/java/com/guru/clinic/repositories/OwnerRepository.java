0525aac87da7c548a38358f6f2b17620700e280b
KobloshAlex <Koblosh.Alex@gmail.com>
2020-06-16 22:29:25 -0400

    JPA services

--- pet-clinic-data/src/main/java/com/guru/clinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/guru/clinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.guru.clinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
---
+++ pet-clinic-data/src/main/java/com/guru/clinic/service/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.guru.clinic.service;
+
+import com.guru.clinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
